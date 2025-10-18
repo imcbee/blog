4693f55794193a33b906ab6974e93acd93501ea3
Ian McBee <imcbee@terpmail.umd.edu>
2025-10-18 15:04:20 -0400

    Quartz sync: Oct 18, 2025, 3:04 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -8,7 +8,7 @@ export const sharedPageComponents: SharedLayout = {
   afterBody: [],
   footer: Component.Footer({
     links: {
-      GitHub: "https://github.com/imcbee",
+      GitHub: "https://github.com/imcbee/blog",
     },
   }),
 }
